7d41078d1ebc66272b422d936e48d2c3effe9e8f
E. Choroba <choroba@matfyz.cz>
2021-12-06 13:38:49 +0100

    Solve day 6: Lanternfish

---
+++ 6a.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use ARGV::OrDATA;
+use List::Util qw{ sum };
+
+use constant DAYS => 80;
+
+my @init = split /,/, <>;
+chomp $init[-1];
+my %fish;
+++$fish{$_} for @init;
+
+for (1 .. DAYS) {
+    my %next;
+    for my $d (keys %fish) {
+        if ($d > 0) {
+            $next{$d - 1} += $fish{$d};
+        } else {
+            $next{8} += $fish{$d};
+            $next{6} += $fish{$d};
+        }
+    }
+    %fish = %next;
+}
+
+say sum(values %fish);
+
+__DATA__
+3,4,3,1,2
