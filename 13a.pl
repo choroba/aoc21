2a50ae4310c297277f119bc103b10c7af16bec2b
E. Choroba <choroba@matfyz.cz>
2021-12-13 08:32:17 +0100

    Solve Day 13: Transparent Origami

---
+++ 13a.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use ARGV::OrDATA;
+
+{   package Paper::Transparent;
+    use Moo;
+    has grid => (is => 'rw', default => sub { {} });
+
+    sub delete { delete $_[0]->grid->{ "$_[1],$_[2]" } }
+    sub dots   { keys %{ $_[0]->grid } }
+
+    sub add {
+        my ($self, @coord) = @_;
+        $coord[0] = join ',', @coord if 2 == @coord;
+        undef  $_[0]->grid->{ $coord[0] };
+    }
+
+    sub fold {
+        my ($self, $axis, $coord) = @_;
+        for my $dot ($self->dots) {
+            my ($x, $y) = split /,/, $dot;
+            if ($axis eq 'y' && $y > $coord) {
+                $self->delete($x, $y);
+                $self->add($x, 2 * $coord - $y);
+            } elsif ($axis eq 'x' && $x > $coord) {
+                $self->delete($x, $y);
+                $self->add(2 * $coord - $x, $y);
+            }
+        }
+    }
+}
+
+my $paper = 'Paper::Transparent'->new;
+while (<>) {
+    chomp;
+    last unless length;
+
+    $paper->add($_);
+}
+
+while (<>) {
+    my ($axis, $coord) = /fold along ([xy])=(\d+)/;
+    $paper->fold($axis, $coord);
+    say scalar $paper->dots;
+    exit
+}
+
+__DATA__
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
