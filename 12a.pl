58611bf924fed8aa4c25d4a60d607221fcb9609f
E. Choroba <choroba@matfyz.cz>
2021-12-14 17:36:01 +0100

    Remove an unneeded line

--- 12a.pl
+++ 12a.pl
@@ -18,7 +18,6 @@ while (my @extendable = grep $paths[$_]->[-1] ne 'end', 0 .. $#paths) {
     for my $ie (reverse @extendable) {
         my $e = splice @paths, $ie, 1;
         for my $next (keys %{ $caves{ $e->[-1] } }) {
-            my @new = (@$e, $next);
             push @paths, [@$e, $next]
                 unless $next =~ /^[[:lower:]]/o
                 && grep $_ eq $next, @$e;
